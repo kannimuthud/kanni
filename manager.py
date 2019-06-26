e4e2b188e535603a27e2630839f588cfe6bd7cb7
kannimuthud <46344606+kannimuthud@users.noreply.github.com>
2019-06-26 14:14:12 +0530

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,6 +1,9 @@
 l1=list(map(int,input().split()))
 l2=list(map(int,input().split()))
 c=0
+def kanni(l):
+    for i in range(0,len(l2)):
+        ifa-
 for i in range(0,len(l1)):
     c=0
     for j in range(0,len(l2)):
@@ -10,5 +13,6 @@ for i in range(0,len(l1)):
                 print(l1[i],"->",l1[j],end="")
             else:
                 print(",",l1[j])
+            kanni(l1[j])
     if c==0:
         print(l1[i],"->","Not a manager")
